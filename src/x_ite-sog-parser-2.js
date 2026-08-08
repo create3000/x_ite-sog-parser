fa1604296a1b72837a2cc32a9ceb2a02435f7f71
Holger Seelig <holger.seelig@gmail.com>
2026-08-09 01:08:28 +0200

    Export GaussianSplats node.

--- src/x_ite-sog-parser-2.js
+++ src/x_ite-sog-parser-2.js
@@ -123,6 +123,9 @@ register (X3D => class SOGParser extends X3D .X3DParser
       transform .children .push (gaussianSplats);
       scene .rootNodes .push (transform);
 
+      scene .addNamedNode (scene .getUniqueName ("GaussianSplats"), gaussianSplats);
+      scene .addExportedNode (scene .getUniqueExportName ("GaussianSplats"), gaussianSplats);
+
       return scene;
    }
 
